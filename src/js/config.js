6e3cfa0ff7043e10cb87a67c1bb33880d0d127f7
Ruby Zhou <138749545+rrubyzhou@users.noreply.github.com>
2025-05-06 21:59:26 -0400

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '1265f071-eb1e-4a8a-82fe-4264ad0b7055';
+export const MODAL_CLOSE_SEC = 2.5;
